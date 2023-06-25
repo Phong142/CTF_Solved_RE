2cb1d0a443ce4722c0e92001f6faf0651f0efd43
Foq <79694403+Phong142@users.noreply.github.com>
2023-06-26 03:22:40 +0700

    CTF

--- CTF_PTIT/Reverse/easyRE/easyRE_Solve.py
+++ CTF_PTIT/Reverse/easyRE/easyRE_Solve.py
@@ -4,27 +4,27 @@ import re
 flag =b"xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 f = [None] * 29
 file = open("save.txt", "w")
-for i in range(32,128):
+for i in range(32,128):                                 #brute force (if array[i] == 0 ---> array[i] = 32-128)
     payload = flag.replace(b"x", chr(i).encode("ascii"))
-    process = subprocess.Popen(["easyRE.exe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    process.stdout.read(36)
-    process.stdin.write(payload + b"\r\n")
+    process = subprocess.Popen(["easyRE.exe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)       ##open file easyRE
+    process.stdout.read(36)                             #read 36 bytes stdout
+    process.stdin.write(payload + b"\r\n")              #write flag stdin
     process.stdin.flush()
-    process.stdout.read(7)
-    char = process.stdout.read(1)
-    while(char!=b"]"):
-        file.write(char.decode("ascii"))
+    process.stdout.read(7)                              #read 7 bytes stdout remove color "\x1b[35m"
+    char = process.stdout.read(1)                       #read 1 byte (array check number)
+    while(char!=b"]"):                                  #compare "]" to remove "\x1b[0m"
+        file.write(char.decode("ascii"))                #write array to .txt
         char = process.stdout.read(1)
     file.write("\n")
 file.close()
 
-with open("save.txt", "r") as file:
+with open("save.txt", "r") as file:                     #read file saved
     cnt = 32
     for line in file:
-      lines = line.strip().split()
+      lines = line.strip().split()                      #split elements
       i = 0
       for word in lines:
-        if(int(word) == 0):
+        if(int(word) == 0):                             #element == 0 --> save to array 
           f[i] = cnt
         i+=1
       cnt+=1
